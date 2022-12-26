c12d4942dbd663cd1dc7d26623dde4890c47d171
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-12-26 11:44:08 -0300

    página sobre criada

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import styles from '../styles/About.module.css';
+
+export default function About() {
+    return (
+        <div className={styles.about}>
+            <h1>Sobre o projeto</h1>
+        </div>
+    )
+}
